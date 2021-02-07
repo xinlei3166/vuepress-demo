6a20865e9e307819874477c1bdc24a39bd731e9e
君惜 <xinlei3166@126.com>
2021-02-07 20:34:24 +0800

    feat: deploy.

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -7,7 +7,7 @@ set -e
 yarn run docs:build
 
 # 进入生成的文件夹
-cd ../docs/.vitepress/dist
+cd ../docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,4 +17,4 @@ git add -A
 git commit -m 'deploy to the gh-pages'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:xinlei3166/vitepress-demo.git master:gh-pages
+git push -f git@github.com:xinlei3166/vuepress-demo.git master:gh-pages
